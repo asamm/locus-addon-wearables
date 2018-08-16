8dd90c1d033b495601a7e97c27924f69cca9a98b
Milan Cejnar <milan.cejnar@asamm.com>
2018-08-16 16:31:14 +0200

    Format time fix.

--- wear/src/main/java/com/asamm/locus/addon/wear/utils/UtilsFormatWear.java
+++ wear/src/main/java/com/asamm/locus/addon/wear/utils/UtilsFormatWear.java
@@ -95,6 +95,9 @@ public class UtilsFormatWear {
         }
     }
 
+    /**
+     * Updated function for time formatting as in stop watch.
+     */
     public static String formatTime(boolean full, long time, boolean withUnits) {
         long hours = time / 3600000;
         long mins = (time - (hours * 3600000)) / 60000;
@@ -114,7 +117,7 @@ public class UtilsFormatWear {
                         formatDouble(mins, 0, 2) + "m:" +
                         formatDouble(sec, 0, 2) + "s";
             } else {
-                return formatDouble(hours, 0, 2) + ":" +
+                return hours + ":" +
                         formatDouble(mins, 0, 2) + ":" +
                         formatDouble(sec, 0, 2);
             }
@@ -137,7 +140,7 @@ public class UtilsFormatWear {
                 if (withUnits) {
                     return hours + "h:" + formatDouble(mins, 0, 2) + "m";
                 } else {
-                    return formatDouble(hours, 0, 2) + ":" +
+                    return hours + ":" +
                             formatDouble(mins, 0, 2) + ":" +
                             formatDouble(sec, 0, 2);
                 }
